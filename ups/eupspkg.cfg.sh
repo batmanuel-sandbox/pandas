8913d423a326f4de396428ccf4b4e7c17378946e
danielsf <scott.f.daniel@gmail.com>
2017-01-23 12:20:06 -0800

    explain why we check for numexpr and bottleneck in a comment in eupspkg.cfg.sh

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,3 +1,13 @@
+# This script will verify that pandas, numexpr, and bottleneck have all
+# been installed. numexpr and bottleneck are prerequisites of some, but
+# not all, pandas functionality. We are checking for all three packages
+# here because
+#    1) It will not always be obvious to developers when they need
+#       numexpr or bottleneck.  All they will see is pandas.
+#
+#    2) We do not want to needlessly create two more stub repositories
+#       with copies of the scripts/check_python_module script in them.
+
 _PANDAS_VERSION="0.19.1"
 _NUMEXPR_VERSION="2.6.0"
 _BOTTLENECK_VERSION="1.2.0"
